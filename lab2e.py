c8a8f1bab74485a5b32471b379fe6462145edc48
Ranjan Ghorsaini <rghorsaini@myseneca.ca>
2025-05-26 16:50:36 -0400

    Completed lab2e.py

---
+++ lab2e.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Author: Ranjan Ghorsaini
+# Author ID: 144843224
+# Date Created: 2025/05/26
+
+# Start at 10
+timer = 10
+
+# Loop until timer is 0
+while timer > 0:
+    print(timer)
+    timer = timer - 1
+
+# After loop ends
+print("blast off!")
+
