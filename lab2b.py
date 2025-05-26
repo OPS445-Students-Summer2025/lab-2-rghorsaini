eab776a8e2ca733b345fdd20eb1cf3df3ae685a7
Ranjan Ghorsaini <rghorsaini@myseneca.ca>
2025-05-26 16:20:26 -0400

    Completed lab2b.py

---
+++ lab2b.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Ask user for name and age
+name = input("Name: ")
+age = input("Age: ")
+
+# Show greeting message
+print("Hi " + name + ", you are " + age + " years old.")
+
